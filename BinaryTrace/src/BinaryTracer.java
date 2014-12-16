91699cd94d58d6ec1f33b02a90e90852f875b051
Alexander.Whitfield <Alexander.Whitfield@MU-C3-23.mullen.pvt.k12.co.us>
2014-12-16 08:57:32 -0700

    Updated some things

--- BinaryTrace/src/BinaryTracer.java
+++ BinaryTrace/src/BinaryTracer.java
@@ -1,25 +1,43 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
+import java.util.Random;
 
 public class BinaryTracer
 	{
+	private static int target;
+	private static int left=0;
+	private static int middle;
+	private static int right;
+	private static int arrayMiddle;
+	private static int userLeft;
+	private static int userRight;
+	private static int userMiddle;
+	private static int userValue;
+	static ArrayList newArray = new ArrayList();
 
 	public static void main(String[] args)
 		{
 		createArray();
-
+		chooseTarget();
 		}
 	public static void createArray()
 		{
-		ArrayList newArray = new ArrayList();
 		int number=0;
 		while (number != -1)
 			{
-			System.out.println("Please enter numbers to be entered into an array. When you are done enter a -1.");
+			System.out.println("Please enter numbers between 1 and 20 to be entered into an array. When you are done enter a -1.");
 			Scanner userInput1 = new Scanner(System.in);
 			number = userInput1.nextInt();
 			if (number == -1)
+				{
+				
+				}
+			else if (number>20)
+				{
+				
+				}
+			else if (number<1)
 				{
 				
 				}
@@ -33,6 +51,30 @@ public class BinaryTracer
 		}
 	public static void chooseTarget()
 		{
-		
+		target = (int) (Math.random()*newArray.size());
+		target = (int) newArray.get(target);
+		System.out.println("The target is: " + target);
+		}
+	public static int findTarget()
+		{
+		int left = 0;
+		int right = newArray.size() - 1;
+		while (left <= right)
+			{
+			int middle = (left + right) / 2;
+			if (target < (int)newArray.get(middle))
+				{
+				right = middle - 1;
+				}
+			else if (target > (int)newArray.get(middle))
+				{
+				left = middle + 1;
+				}
+			else
+				{
+				return middle;
+				}
+			}
+		return -1;
 		}
 	}
